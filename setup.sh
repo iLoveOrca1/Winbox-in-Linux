1a43e3ff539e067b8927138a6a2185380fc2a076
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2025-11-25 17:50:57 +0800

    updating winbox

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+DIRECTORY="/home/$username/.local/share/applications/winbox"
 read -p "Please enter your user: " username 
+
 chmod +x ./winbox
-mkdir /home/$username/.local/share/applications/winbox
+if [ ! -e $DIRECTORY ]; then
+	mkdir /home/$username/.local/share/applications/winbox
+fi
 cp -f ./assets/winbox.png /usr/share/icons/winbox.png
 cp -f ./assets/winbox.desktop /home/$username/.local/share/applications/winbox/winbox.desktop
 echo "Creating desktop entry for the user $username"
